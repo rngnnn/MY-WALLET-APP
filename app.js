a5679d4bcae9c5d4ee31a8490dfa6761aa90df2f
rngnnn <rngnnnclk@gmail.com>
2024-06-14 09:40:16 +0200

    RENEW

--- app.js
+++ app.js
@@ -1,43 +1,19 @@
-let tarih = document.getElementById("date");
-    let today = new Date();
-    let year = today.getFullYear();
-    let month = ('0' + (today.getMonth() + 1)).slice(-2); // Ayı iki basamaklı yapmak için
-    let day = ('0' + today.getDate()).slice(-2); // Günü iki basamaklı yapmak için
-    let formattedDate = `${year}-${month}-${day}`;
-    tarih.value = formattedDate;
+//^ Selectors
 
+const ekleBtn = document.getElementById("ekle-btn")
+const gelirInput = document.getElementById("gelir-input")
+const ekleFormu = document.getElementById("ekle-formu")
 
+//^ variables
+let gelirler = 0
 
-    
-   let mainArea= document.querySelector(".mainArea")
-   let miktarInput =document.getElementById("harcama")
-   let harcamaSebeb=document.getElementById("harcamaSebeb")
+//? Ekle Formu
 
-
- 
-let harcamaListesi=[]
-
-   mainArea.addEventListener("submit",(e)=>{
+ekleFormu.addEventListener("submit", (e)=>{
     e.preventDefault()
-    console.log("test",e);
+    gelirler = gelirler + Number(gelirInput.value)
+    console.log(gelirler)
+    // gelirInput.value = ''
+    ekleFormu.reset()
 
- const yeniHarcama={
-
-    tarih: tarih.value,
-    miktar: miktarInput.value,
-    harcamaSebebi: harcamaSebeb.value,
-    id: new Date().getTime()
-
- }
-
- harcamaListesi.push(yeniHarcama)
- localStorage.setItem("harcamalar",JSON.stringify('harcamaListesi'))
-
- 
-
-   })
-
-
-   harcamaTablosunaEkle(yeniHarcama);
-   mainArea.reset();
-   tarih.value = formattedDate;
+})
