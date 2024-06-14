6dcacf42906098d70991342995fe0f0d048f321d
rngnnn <rngnnnclk@gmail.com>
2024-06-14 10:22:05 +0200

    value

--- app.js
+++ app.js
@@ -13,13 +13,11 @@ const kalanTd = document.getElementById("kalan")
 let gelirler = 0
 
 
-
-//? Haracama Formu
-
-const haracamaFormu=document.getElementById("harcama-formu")
-const harcamaAlaniInput=document.getElementById("harcama-alani")
-const tarihInput=document.getElementById("tarih")
-const harcamaInput=document.getElementById("miktar")
+//^ Harcama Formu
+const harcamaFormu = document.getElementById("harcama-formu")
+const harcamaAlaniInput = document.getElementById("harcama-alani")
+const tarihInput = document.getElementById("tarih")
+const harcamaInput = document.getElementById("miktar")
 
 
 //? Ekle Formu
@@ -27,15 +25,29 @@ const harcamaInput=document.getElementById("miktar")
 ekleFormu.addEventListener("submit", (e)=>{
     e.preventDefault()
     gelirler = gelirler + Number(gelirInput.value)
-    console.log(gelirler)
+    // console.log(gelirler)
     // gelirInput.value = ''
     ekleFormu.reset()
     localStorage.setItem("gelirler", gelirler)
-    gelirinizTd.textContent = gelirler
+    gelirinizTd.textContent = new Intl.NumberFormat().format(gelirler)  
 
 })
 
 window.addEventListener("load",()=>{
-    gelirler = Number(localStorage.getItem("gelirler"))
-    gelirinizTd.textContent = gelirler
+    gelirler = Number(localStorage.getItem("gelirler")) || 0
+    gelirinizTd.textContent = new Intl.NumberFormat().format(gelirler)  
+})
+
+
+harcamaFormu.addEventListener("submit", (e)=>{
+    e.preventDefault()
+
+    const yeniHarcama={
+
+      id:new Date().getTime(), //?sistem saatini milisaniye olarak verir
+      tarih:new Date(tarihInput.value).toLocaleDateString(),
+      miktar:harcamaInput.value,
+      alan:harcamaAlaniInput.value,
+    }
+    
 })
