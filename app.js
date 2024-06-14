92504c8ab208e81a91e17e6f0582be7d2deafeb8
rngnnn <rngnnnclk@gmail.com>
2024-06-14 09:52:42 +0200

    INCOME

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const ekleBtn = document.getElementById("ekle-btn")
 const gelirInput = document.getElementById("gelir-input")
 const ekleFormu = document.getElementById("ekle-formu")
 
+//^ Hesap Tablosu
+const gelirinizTd = document.getElementById("geliriniz")
+const giderinizTd = document.getElementById("gideriniz")
+const kalanTd = document.getElementById("kalan")
+
 //^ variables
 let gelirler = 0
 
@@ -15,5 +20,12 @@ ekleFormu.addEventListener("submit", (e)=>{
     console.log(gelirler)
     // gelirInput.value = ''
     ekleFormu.reset()
+    localStorage.setItem("gelirler", gelirler)
+    gelirinizTd.textContent = gelirler
 
+})
+
+window.addEventListener("load",()=>{
+    gelirler = Number(localStorage.getItem("gelirler"))
+    gelirinizTd.textContent = gelirler
 })
